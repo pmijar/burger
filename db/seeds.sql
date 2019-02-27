3f22af39bfbd9d76d9ab9cefd77cefc0b17bf9c8
pmijar <prashanth.mijar@gmail.com>
2019-02-26 22:54:19 -0500

    Code for burger with the changes in Controller, Models and .js files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Happy Meal Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
